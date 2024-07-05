43fd54a7fc7f866b5c52031efa7fea0e5a77cc39
Mayank Mahajan <mayankpmahajan@gmail.com>
2024-07-05 17:06:24 +0530

    added authentication

---
+++ api/routes/post.route.js
@@ -0,0 +1,27 @@
+import express from 'express';
+
+const router = express.Router();
+
+
+// router.get("/test", (req,res)=>{
+//     console.log("router works");
+//     res.send("yes");
+// })
+
+// router.post("/test", (req,res)=>{
+//     console.log("router works");
+//     res.send("yes");
+// })
+
+// router.put("/test", (req,res)=>{
+//     console.log("router works");
+//     res.send("yes");
+// })
+
+// router.delete("/test", (req,res)=>{
+//     console.log("router works");
+//     res.send("yes");
+// })
+
+
+export default router;
